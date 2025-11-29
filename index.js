741e8043036179c1fb0114cdda0102c1c305bc75
Alexx <passosgabriela853@gmail.com>
2025-11-29 01:26:46 -0300

    node e planejamento

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+Etapas do código:
+
+1. Leitura da Matriz;
+2. Estratégia:
+    - Ponto de Sela;
+    - Mista 2x2;
+    - Mista MxN;
+3. Impressão da Interpretação:
+    - Movimento Fixo se houver Ponto de Sela;
+    - Movimento com Probabilidades se for Mista 2x2.
+    - Movimento com Probabilidades se for Mista MxN.
+*/ 
+
